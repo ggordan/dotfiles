f689e5a235f14662c5d84a84eb8f28e8772b6a01
Gordan Grasarevic <ggordan@hurricane.local>
2017-08-21 13:35:18 +0100

    update

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,5 @@
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
+export PATH="$HOME/swipe/go/bin:$HOME/Google/appengine/gopath/bin:$HOME/Google/appengine:$HOME/cassandra/bin:./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+
+export GOPATH=$HOME/Code/go
+export PATH="$GOPATH/bin:/usr/local/opt/go/libexec/bin:$PATH"
--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -62,6 +62,7 @@ battery_status() {
 }
 
 export PROMPT=$'\n$(battery_status)in $(directory_name) $(git_dirty)$(need_push)\n› '
+
 set_prompt () {
   export RPROMPT="%{$fg_bold[cyan]%}%{$reset_color%}"
 }
