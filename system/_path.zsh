b2b774110047c941fe39a223a0e96819da24d803
Gordan Grasarevic <me@ggordan.com>
2019-03-24 18:11:41 +0000

    Removing mac specific things

--- system/_path.zsh
+++
@@ -1,5 +0,0 @@
-export PATH="$HOME/swipe/go/bin:$HOME/Google/appengine/gopath/bin:$HOME/Google/appengine:$HOME/cassandra/bin:./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
-export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
-
-export GOPATH=$HOME/Code/go
-export PATH="$GOPATH/bin:/usr/local/opt/go/libexec/bin:$PATH"
